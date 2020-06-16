e238ecbc9239622abc4eeb746536a52da48bf45d
Mornely Tom <kirtan321@gmail.com>
2020-06-17 00:07:11 +0300

    added firebase authorization. changed authorization and settings logout.

--- app/src/main/java/com/mornelytom/telegram/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/mornelytom/telegram/ui/fragments/EnterCodeFragment.kt
@@ -1,27 +1,39 @@
 package com.mornelytom.telegram.ui.fragments
 
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.PhoneAuthProvider
+import com.mornelytom.telegram.MainActivity
 import com.mornelytom.telegram.R
+import com.mornelytom.telegram.activities.AuthorizationActivity
+import com.mornelytom.telegram.utilits.AUTH
 import com.mornelytom.telegram.utilits.AppTextWatcher
+import com.mornelytom.telegram.utilits.replaceActivity
 import com.mornelytom.telegram.utilits.showToast
 import kotlinx.android.synthetic.main.fragment_enter_code.*
 
-class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
+class EnterCodeFragment(val phone: String, val id: String) :
+    Fragment(R.layout.fragment_enter_code) {
 
     override fun onStart() {
         super.onStart()
+        (activity as AuthorizationActivity).title = phone
         auth_input_code.addTextChangedListener(AppTextWatcher {
             val string = auth_input_code.text.toString()
             if (string.length == 6) {
-                verifyCode()
+                enterCode()
             }
         })
     }
 
-    fun verifyCode() {
-        showToast("Ok")
+    private fun enterCode() {
+        val code = auth_input_code.text.toString()
+        val credential = PhoneAuthProvider.getCredential(id, code)
+        AUTH.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                (activity as AuthorizationActivity).replaceActivity(MainActivity())
+            } else {
+                showToast(it.exception.toString())
+            }
+        }
     }
 }
--- app/src/main/java/com/mornelytom/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/mornelytom/telegram/ui/fragments/SettingsFragment.kt
@@ -2,7 +2,12 @@ package com.mornelytom.telegram.ui.fragments
 
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import com.mornelytom.telegram.MainActivity
 import com.mornelytom.telegram.R
+import com.mornelytom.telegram.activities.AuthorizationActivity
+import com.mornelytom.telegram.utilits.AUTH
+import com.mornelytom.telegram.utilits.replaceActivity
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
@@ -13,5 +18,15 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu_exit -> {
+                AUTH.signOut()
+                (activity as MainActivity).replaceActivity(AuthorizationActivity())
+            }
+        }
+        return true
     }
 }
