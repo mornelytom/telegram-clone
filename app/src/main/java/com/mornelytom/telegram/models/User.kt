125c679b6770e68add488285c99d7ec799c1dae6
Mornely Tom <kirtan321@gmail.com>
2020-07-30 22:40:40 +0300

    added state enum.

--- app/src/main/java/com/mornelytom/telegram/models/User.kt
+++ app/src/main/java/com/mornelytom/telegram/models/User.kt
@@ -5,7 +5,7 @@ class User(
     var username: String = "",
     var fullname: String = "",
     var bio: String = "",
-    var status: String = "",
+    var state: String = "",
     var phone: String = "",
-    var photoUrl: String = ""
+    var photoUrl: String = "empty"
 )
